00c38b33d45730eb974895c724c7f72b127d77ad
sashintulsiram <50657550+sashintulsiram@users.noreply.github.com>
2021-07-17 13:58:30 +0200

    Update 3cxmoverecdata.sh

--- 3cxmoverecdata.sh
+++ 3cxmoverecdata.sh
@@ -37,8 +37,8 @@ IFS=$'\n'
 #       echo "$DST" >>./getccrecdata.log
         CID=$(echo $filename | cut -f6 -d-)
         CALLID="${CID%.*}"
-          if [ "$DST" -gt "$SRC" ]; then DIR=INCOMING; EXT=$SRC; PHN=$DST
-          else DIR=OUTGOING; EXT=$DST; PHN=$SRC
+          if [ "$SRC" -gt "$DST" ]; then DIR=INCOMING; EXT=$DST; PHN=$SRC
+          else DIR=OUTGOING; EXT=$SRC; PHN=$DST
           fi
           if [ -z "$EXT" ]; then EXT=anonymous;
           fi
